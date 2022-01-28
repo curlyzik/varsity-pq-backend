b39f4a40ce3cf9212e16e9a0bae7696a09467aec
curlyzik <curlyzik@gmail.com>
2022-01-28 15:39:57 +0100

    configure heroku

--- uni_project/settings/production.py
+++ uni_project/settings/production.py
@@ -1,12 +1,17 @@
 from .base import *
+import django_heroku
 
 DEBUG = False
 
+# Amazon S3 Configurations
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
 AWS_ACCESS_KEY_ID = env(('AWS_ACCESS_KEY_ID'))
 AWS_SECRET_ACCESS_KEY = env(('AWS_SECRET_ACCESS_KEY'))
 AWS_STORAGE_BUCKET_NAME = env(('AWS_STORAGE_BUCKET_NAME'))
 AWS_QUERYSTRING_AUTH = False
 
+# Static file configuration
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+# heroku configuration
+django_heroku.settings(locals())
