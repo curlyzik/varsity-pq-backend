afb351bdf2fe402029e1788ec63c643e29a1875e
curlyzik <curlyzik@gmail.com>
2022-02-14 06:14:27 +0100

    validation

--- users/views.py
+++ users/views.py
@@ -13,13 +13,18 @@ User = get_user_model()
 
 class VolunteerRequest(APIView):
     def post(self, request):
-        print(request)
         full_name = request.data.get("full_name")
         university = request.data.get("university")
         faculty = request.data.get("faculty")
         department = request.data.get("department")
         year = request.data.get("year")
         email = request.data.get("email")
+
+        if User.objects.filter(email=email).exists():
+            return Response(
+                {"detail": "User with this email already exists"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
         volunteer = User.objects.create(
             full_name=full_name,
@@ -33,7 +38,6 @@ class VolunteerRequest(APIView):
             random.choice(string.ascii_uppercase + string.digits) for _ in range(15)
         )
         volunteer.set_password(password)
-        volunteer.is_staff = True
         volunteer.save()
 
         serializer = UserSerializer(volunteer)
@@ -41,11 +45,3 @@ class VolunteerRequest(APIView):
             status=status.HTTP_201_CREATED,
             data={"password": password, "data": serializer.data},
         )
-
-
-# class GetUsers(APIView):
-#     permission_classes = [permissions.AllowAny]
-#     def get(self, request):
-#         users = User.objects.all()
-#         serializer = UserSerializer(users, many=True)
-#         return Response(serializer.data)
