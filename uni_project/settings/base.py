48db66ef6b01c4e50164cbb50beb805e5c93b736
curlyzik <curlyzik@gmail.com>
2022-03-10 01:25:49 +0100

    error debug

--- uni_project/settings/base.py
+++ uni_project/settings/base.py
@@ -157,8 +157,17 @@ SITE_ID = 1
 ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_EMAIL_REQUIRED = True
 
-# SEND EMAIL USING SENDGRID ANYMAIL
-EMAIL_BACKEND = 'anymail.backends.sendgrid.EmailBackend'
-ANYMAIL = {
-    "SENDGRID_API_KEY": env("SENDGRID_API_KEY")
-}
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
+# EMAIL_HOST_PASSWORD = env("SENDGRID_API_KEY")
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+
+# EMAIL_BACKEND = 'anymail.backends.sendgrid.EmailBackend'
+# ANYMAIL = {
+#     "SENDGRID_API_KEY": env("SENDGRID_API_KEY")
+# }
+
+EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
+SENDGRID_API_KEY = env(("SENDGRID_API_KEY"))
+SENDGRID_SANDBOX_MODE_IN_DEBUG = False
