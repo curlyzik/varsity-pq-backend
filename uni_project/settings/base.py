72341b706390601b0d6bb4e9a22fb326f39f23c9
curlyzik <curlyzik@gmail.com>
2022-03-10 15:31:57 +0100

    setup sendgrid

--- uni_project/settings/base.py
+++ uni_project/settings/base.py
@@ -2,7 +2,6 @@ import os
 import environ
 from datetime import timedelta
 from pathlib import Path
-from mailjet_rest import Client
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
