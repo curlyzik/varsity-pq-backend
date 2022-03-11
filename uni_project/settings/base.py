a1d8a5305e86c3a03229590a1b427917cd9ffda0
curlyzik <curlyzik@gmail.com>
2022-03-11 13:02:40 +0100

    little fix

--- uni_project/settings/base.py
+++ uni_project/settings/base.py
@@ -154,6 +154,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = env(("EMAIL_HOST"))
 EMAIL_HOST_USER = env(("EMAIL_HOST_USER"))
 EMAIL_HOST_PASSWORD = env(("EMAIL_HOST_PASSWORD"))
-EMAIL_PORT = 2525
+EMAIL_PORT = 25
 EMAIL_USE_TLS = True
 
