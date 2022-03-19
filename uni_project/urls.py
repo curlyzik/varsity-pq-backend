9fb262f78bc88ead4426c0384749feb488e3db71
Nzekwe Isaac Edidiong <curlyzik@gmail.com>
2022-03-19 05:33:14 +0100

    removed drf

--- uni_project/urls.py
+++ uni_project/urls.py
@@ -3,24 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version="v1",
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
-
-urlpatterns = [...]
 urlpatterns = (
     [
         path("admin/", admin.site.urls),
@@ -28,14 +10,6 @@ urlpatterns = (
         path("users/", include("users.urls")),
         path("dj-rest-auth/", include("dj_rest_auth.urls")),
         path("auth/", include("djoser.urls")),
-        path(
-            "swagger/",
-            schema_view.with_ui("swagger", cache_timeout=0),
-            name="schema-swagger-ui",
-        ),
-        path(
-            "redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
-        ),
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
